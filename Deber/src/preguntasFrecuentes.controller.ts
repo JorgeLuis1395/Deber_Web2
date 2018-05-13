473481cbb8bbd316e0933ccc576bbbdfb9a0e9db
JorgeLuis1395 <carrillojorge1395@hotmail.com>
2018-05-13 16:22:33 -0500

    Cambios en el archivo Preguntas

--- Deber/src/preguntasFrecuentes.controller.ts
+++ Deber/src/preguntasFrecuentes.controller.ts
@@ -2,18 +2,18 @@ import {Controller, Get, Post, Req, Res} from "@nestjs/common";
 
 const fs =require('fs');
 let preguntasHtml=fs.readFileSync(__dirname+'/html/preguntasFrecuentes.html', 'utf8');
-@Controller('PreguntasFrecuentes')
+@Controller()
 export class PreguntasFrecuentesController {
 
     preguntas_frecuentes=[];
     @Post('preguntasIngreso')
     anadirPreguntas(@Req() request, @Res() response){
         const parametrosConsulta=request.query;
-         this.preguntas_frecuentes.push(new preguntasfrecuentesParametros(parametrosConsulta.pregunta, parametrosConsulta.respuesta));
-        this.preguntas_frecuentes.forEach(value => {
-            preguntasHtml=preguntasHtml.concat('<h1> Pregunta </h1> ',value.pregunta);
-            preguntasHtml=preguntasHtml.concat('<p> Respuesta</p>', value.respuesta);
-        });
+         this.preguntas_frecuentes.push(new preguntasfrecuentesParametros(parametrosConsulta.preguntas1, parametrosConsulta.respuestas));
+        console.log(request.preguntas1);
+            preguntasHtml=preguntasHtml.concat('<h1> Pregunta </h1> ',request.preguntas1);
+            preguntasHtml=preguntasHtml.concat('<p> Respuesta</p>', request.respuestas);
+
         return response.send(this.preguntas_frecuentes);
     }
 
@@ -26,8 +26,8 @@ export class PreguntasFrecuentesController {
 }
 
 class preguntasfrecuentesParametros{
-    constructor(public pregunta:string,
-                public respuesta: string){
+    constructor(public preguntas1: string,
+                public respuestas: string){
     }
 
 }
